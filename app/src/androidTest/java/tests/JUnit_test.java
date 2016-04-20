7c489387f91554be6ea929a2feb62cc828a6eee2
Kenny Law <kennylaw95@gmail.com>
2016-04-20 07:57:19 -0700

    Fixed errors

--- app/src/androidTest/java/tests/JUnit_test.java
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -9,20 +9,20 @@ import com.android.lab3.R;
 /**
  * Created by Simon on 4/19/2016.
  */
-public class JUnit_test extends ActivityInstrumentationTestCase2 {
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
 
     MainActivity mainActivity;
     public JUnit_test() {
 
-        super(MainActivity.class)
+        super(MainActivity.class);
     }
 
     public void test_first() {
-        mainActivity = (MainActivity) getActivity();
+        mainActivity = getActivity();
 
-        TextView textView = (TextView) mainActivity.findViewbyId(R.id.tv1);
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
         String tester = textView.getText().toString();
 
-        assertEquals("Bye World", tester);
+        assertEquals("Hello World", tester);
     }
 }
