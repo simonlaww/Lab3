0f8b592aad829bb946120a11afac3ecd5f550c99
Kenny Law <kennylaw95@gmail.com>
2016-04-20 01:15:06 -0700

    Fixed mainactivity

--- app/src/androidTest/java/tests/JUnit_test.java
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -4,19 +4,21 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
 
 import com.android.lab3.MainActivity;
+import com.android.lab3.R;
 
 /**
  * Created by Simon on 4/19/2016.
  */
 public class JUnit_test extends ActivityInstrumentationTestCase2 {
 
+    MainActivity mainActivity;
     public JUnit_test() {
 
         super(MainActivity.class)
     }
 
     public void test_first() {
-        mainActivity = getActivity();
+        mainActivity = (MainActivity) getActivity();
 
         TextView textView = (TextView) mainActivity.findViewbyId(R.id.tv1);
         String tester = textView.getText().toString();
